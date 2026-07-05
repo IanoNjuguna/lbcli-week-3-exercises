4ce7d63b2ca8d255efdaf528fda84afb1d70b19a
IanoNjuguna <inc3099@gmail.com>
2026-07-05 12:54:50 +0300

    create a native segwit address and get the public key

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,6 @@
+#!/usr/bin/bash
 # Create a native segwit address and get the public key from the address.
+WALLET="builderswallet"
+SEGWIT_ADDR=$(bitcoin-cli -regtest -rpcwallet="$WALLET" getnewaddress "" bech32)
+bitcoin-cli -regtest -rpcwallet="$WALLET" getaddressinfo "$SEGWIT_ADDR" | jq -r '.pubkey'
+
