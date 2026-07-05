fa92313978ca3620255e64e39d6aa50f950ed1e3
IanoNjuguna <inc3099@gmail.com>
2026-07-05 16:00:38 +0300

    remove 256 checksum from the solution

--- submission/03.sh
+++ submission/03.sh
@@ -12,9 +12,5 @@ PUB_KEYS=$(echo "$DECODE_TX" | jq -r '.vin[].txinwitness[1]')
 FORMATTED_KEYS=$(echo "$PUB_KEYS" | sort | jq -R . | jq -s)
 
 # create multisig address
-MULTISIG_ADDR=$(bitcoin-cli -regtest createmultisig 1 "$FORMATTED_KEYS" 'p2sh-segwit' | jq -r '.redeemScript')
+MULTISIG_ADDR=$(bitcoin-cli -regtest createmultisig 1 "$FORMATTED_KEYS" 'p2sh-segwit' | jq -r '.address')
 
-#echo "$DECODE_TX"
-
-echo "DEBUG: My Address: $MULTISIG_ADDR"
-echo -n "$MULTISIG_ADDR" | tr -d '\n' | sha256sum
